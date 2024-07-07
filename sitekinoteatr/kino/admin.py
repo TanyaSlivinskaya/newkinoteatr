2c142f2640cc5ca05a3df1a5c88cd22bf9f6c0fa
TanyaSlivinskaya <tanya.slivinskaya.2000@mail.ru>
2024-07-07 19:06:46 +0300

    new files

--- sitekinoteatr/kino/admin.py
+++ sitekinoteatr/kino/admin.py
@@ -1,3 +1,46 @@
 from django.contrib import admin
+from .models import Genre, Director, Actor, Movie, Session, Ticket
+from django.core.management import call_command
 
-# Register your models here.
+class GenreAdmin(admin.ModelAdmin):
+    search_fields = ['name']
+
+class DirectorAdmin(admin.ModelAdmin):
+    list_display = ['name', 'photo']
+    search_fields = ['name']
+
+class ActorAdmin(admin.ModelAdmin):
+    list_display = ['name', 'photo']
+    search_fields = ['name']
+
+class ActorInline(admin.TabularInline):
+    model = Movie.actor.through
+    extra = 1
+
+class SessionAdmin(admin.ModelAdmin):
+    list_display = ['movie', 'time', 'hall']
+    search_fields = ['movie__title']
+    list_filter = ['time', 'hall']
+
+class TicketAdmin(admin.ModelAdmin):
+    list_display = ['user', 'session', 'seat']
+    search_fields = ['user__username', 'session__movie__title']
+    list_filter = ['session__time']
+
+def fill_fake_data(modeladmin, request, queryset):
+    call_command('fill_fake_data')
+
+fill_fake_data.short_description = 'Заполнить фейковыми данными'
+
+class MovieAdmin(admin.ModelAdmin):
+    list_display = ['title', 'genre', 'director']
+    search_fields = ['title', 'description']
+    list_filter = ['genre', 'director']
+    actions = [fill_fake_data]
+
+admin.site.register(Genre, GenreAdmin)
+admin.site.register(Director, DirectorAdmin)
+admin.site.register(Actor, ActorAdmin)
+admin.site.register(Movie, MovieAdmin)
+admin.site.register(Session, SessionAdmin)
+admin.site.register(Ticket, TicketAdmin)
--- sitekinoteatr/kino/models.py
+++ sitekinoteatr/kino/models.py
@@ -1,3 +1,55 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Genre(models.Model):
+    name = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.name
+
+
+class Director(models.Model):
+    name = models.CharField(max_length=100)
+    photo = models.ImageField(upload_to='media/photodirector')
+
+    def __str__(self):
+        return self.name
+
+
+class Actor(models.Model):
+    name = models.CharField(max_length=100)
+    photo = models.ImageField(upload_to='media/photoactor')
+
+    def __str__(self):
+        return self.name
+
+
+class Movie(models.Model):
+    title = models.CharField(max_length=100)
+    description = models.TextField(max_length=500)
+    genre = models.ForeignKey(Genre, on_delete=models.CASCADE)
+    director = models.ForeignKey(Director, on_delete=models.CASCADE)
+    actor = models.ManyToManyField(Actor)
+    poster = models.ImageField(upload_to='media/movies')
+
+    def __str__(self):
+        return self.title
+
+
+class Session(models.Model):
+    movie = models.ForeignKey(Movie, on_delete=models.CASCADE)
+    time = models.DateTimeField()
+    hall = models.CharField(max_length=100)
+
+    def __str__(self):
+        return f'{self.movie.title} - {self.time}'
+
+
+class Ticket(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    session = models.ForeignKey(Session, on_delete=models.CASCADE)
+    seat = models.CharField(max_length=10)
+
+    def __str__(self):
+        return f"{self.user.username} - {self.session.movie.title} - {self.seat}"
